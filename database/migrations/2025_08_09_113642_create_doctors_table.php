543431c5f0047a6bc95718bbfa279b0be29bd2f0
ALBaraa <albaraawork3@gmail.com>
2025-08-09 14:51:13 +0300

    Initialize Laravel application structure with routing, testing, and storage configurations
    - Added console command for inspiring quotes in routes/console.php
    - Created web route for the home page in routes/web.php
    - Configured .gitignore files for storage directories to exclude unnecessary files
    - Implemented basic feature and unit tests in tests/Feature and tests/Unit
    - Set up Vite configuration for asset management with Laravel and Tailwind CSS

---
+++ database/migrations/2025_08_09_113642_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('clinic_id')->constrained('clinics')->onDelete('cascade');
+            $table->foreignId('clinic_department_id')->constrained('clinic_department')->onDelete('cascade');
+            $table->foreignId('staff_id')->constrained('staff')->onDelete('cascade');
+            $table->foreignId('speciality_id')->constrained('specialties')->onDelete('cascade');
+            $table->string('qualification')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
